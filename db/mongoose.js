d33e4fa9aad98ff1413a591c6a8e445b7f857786
Anand Kumar <vaidhanianandkumar@gmail.com>
2020-10-17 14:05:18 +0530

    changed database connections

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,12 +4,18 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/jobs', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    console.log('connected Local mongodb succesfully');
+mongoose.connect('mongodb+srv://capturemoment:timon&pumba@capturemoment-o6obu.mongodb.net/jobs?retryWrites=true&w=majority', { useNewUrlParser: true }).then(() => {
+    console.log('connected mongodb succesfully');
 }).catch((e) => {
     console.log('error while connecting to database');
     console.log(e);
 });
+// mongoose.connect('mongodb://localhost:27017/jobs', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+//     console.log('connected Local mongodb succesfully');
+// }).catch((e) => {
+//     console.log('error while connecting to database');
+//     console.log(e);
+// });
 
 //to prevent deprication warning
 
